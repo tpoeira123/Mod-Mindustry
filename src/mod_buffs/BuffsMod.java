0b63f139bda62fda040ccec6edcf93779b14c6d7
tpoeira123 <tpoeira2005@gmail.com>
2025-11-19 00:41:33 +0000

    start mod

---
+++ src/mod_buffs/BuffsMod.java
@@ -0,0 +1,17 @@
+package mod_buffs;
+
+import arc.*;
+import arc.util.*;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.type.StatusEffect;
+import mindustry.ui.dialogs.*;
+
+public class BuffsMod extends Mod{
+    private static final int DELTA_POINTS = 50;
+
+    private static final int POINTS_WHEN_DEAD = 0;
+
+    private StatusEffect[] buffs;
+
+}
